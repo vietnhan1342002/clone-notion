752a6d66c832725331ac063c0ea98fdfced73d82
vietnhan1342002 <cudum13402nhan@gmail.com>
2025-11-24 22:34:17 +0700

    fix(setup): re-setup Tailwind, ShadCN and delete husky and commitlint

--- src/components/Button.tsx
+++
@@ -1,4 +0,0 @@
-type Props = { label: string }
-export function Button({ label }: Props) {
-  return <button>{label}</button>
-}
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.tsx'
 import { store } from './store/index.ts'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
